ffc4b94c42d062774d64d4e30b4e376ed86190cc
jaideepvinayak <2410080027@klh.edu.in>
2025-10-17 14:27:16 +0530

    feat: Update API URL for production

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // This is the URL of the API you are running locally
-    const API_URL = 'http://127.0.0.1:5000/api/news';
+    const API_URL = 'http://Jaideepvinayak.pythonanywhere.com/api/news';
 
     const container = document.getElementById('headlines-container');
 
